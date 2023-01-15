72878c0e402248ee32b2ff9d30e483e4fa27424d
waldemar <vova108vova@gmail.com>
2023-01-15 21:27:12 +0300

    POST forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
+import forms from './modules/forms';
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs();
+    forms();
 });
---
+++ src/js/modules/forms.js
@@ -0,0 +1,56 @@
+const formsFn = () => {
+    const forms = document.querySelectorAll('form');
+    const inputs = document.querySelectorAll('input');
+    const inputsPhone = document.querySelectorAll('input[name="user_phone"]');
+    inputsPhone.forEach((input) => {
+        input.addEventListener('input', () => {
+            input.value = input.value.replace(/\D/, '');
+        });
+    });
+    const messege = {
+        loading: "Laoding...",
+        success: "We will call YOU soon!",
+        failure: "Somesing's wrong!",
+    };
+    const clearInputs = () => {
+        inputs.forEach(input => input.value = '');
+    };
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const postData = async (url, data) => {
+        const dataObject = {};
+        data.forEach((value, key) => {
+            dataObject[key] = value;
+            console.log(key, value);
+        });
+        console.log(JSON.stringify(dataObject));
+        document.querySelector('.status').textContent = messege.loading;
+        const response = await fetch(url , {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json;charset=UTF-8",
+            },
+            body: JSON.stringify(dataObject),
+        });
+        return await response.json();
+    };
+    forms.forEach((form) => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const messegeBox = document.createElement('div');
+            messegeBox.classList.add('status');
+            form.appendChild(messegeBox);
+            const formData = new FormData(form);
+            postData(url, formData)
+                .then(response => {
+                console.log(response);
+                messegeBox.textContent = messege.success;
+            })
+                .catch(() => messegeBox.textContent = messege.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => messegeBox.remove(), 5000);
+                });
+        });
+    });
+};
+export default formsFn;
